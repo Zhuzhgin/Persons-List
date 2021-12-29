473cb78483e8a4abcd64b927f3e47b6b4cb2523d
Zhuzhgin <artemfox@list.ru>
2021-12-29 23:01:00 +0500

    2

--- Persons List/FullInfoTableViewController.swift
+++ Persons List/FullInfoTableViewController.swift
@@ -24,13 +24,34 @@ class FullInfoTableViewController: UITableViewController {
         personsArray.count
     }
     
-    
+  
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        "aaa"
+        "\(personsArray[section].name) \(personsArray[section].surName)"
     }
-    sellAtinde
+
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        
+       // print(indexPath.row)
+        let person = personsArray[indexPath.section ]
+        var content = cell.defaultContentConfiguration()
+        
+        switch indexPath.row {
+        case 0:
+            content.text = person.phone
+           
+            
+        default: content.text = person.eMail
+        }
+        cell.contentConfiguration = content
+        
+        return cell
     }
 }
 
